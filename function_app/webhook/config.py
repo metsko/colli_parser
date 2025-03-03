83862ac9a94599d40f4f40b4e1265de1df15b564
Maarten De Groodt <maarten.degroodt@datawizards.io>
2025-03-04 00:32:27 +0100

    feature/register-in-splitwisr: infra & function code

--- function_app/webhook/config.py
+++ function_app/webhook/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    API_TOKEN = os.environ.get("CHATGPT_API_TOKEN")  # Set your API token as an environment variable
+    API_TOKEN = os.environ.get("CHATGPT_API_TOKEN")
     if not API_TOKEN:
         raise ValueError("No API token found. Please set the CHATGPT_API_TOKEN environment variable.")
 
